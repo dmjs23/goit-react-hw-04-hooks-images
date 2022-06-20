e86f1639fdc652cfd9cb2044c87308af80f4f493
Kamil Przebieracz <dmjs23@gmail.com>
2022-06-20 18:40:41 +0200

    Update api.js

--- src/components/services/api.js
+++ src/components/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const getImages = (query = '', page = 1) => {
-  const key = '26531596-66f70a56847dae6fbc6ddebb0';
+  const key = '15302977-99a8e51ff55a3c0e02bc236e3';
   const path = `https://pixabay.com/api/?q=${query}&page=${page}&key=${key}&image_type=photo&orientation=horizontal&per_page=12`;
   return axios.get(path);
 };
